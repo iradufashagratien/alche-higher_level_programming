15383669055a54cc4e103e0f3550edd4821d6d74
iradufashagratien <g.iradufash@alustudent.com>
2026-09-10 03:27:52 -0700

    Fix all Python tasks

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-"""Module for LockedClass"""
-
-
 class LockedClass:
-    """Class that only allows first_name attribute"""
-
-    def __setattr__(self, name, value):
-        """Restrict attribute assignment to first_name only"""
-        if name == 'first_name':
-            self.__dict__[name] = value
-        else:
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+    __slots__ = ['first_name']
