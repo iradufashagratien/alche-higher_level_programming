6d4417a1f388450d7a420569878dc64fb7756b04
iradufashagratien <g.iradufash@alustudent.com>
2026-09-10 03:41:37 -0700

    Fix all Python tasks

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -3,10 +3,13 @@
 
 
 class LockedClass:
-    """Locked class restricts attributes"""
+    """Locked class that restricts attributes"""
 
     def __setattr__(self, name, value):
-        if name != 'first_name':
-            msg = "'LockedClass' object has no attribute '" + name + "'"
-            raise AttributeError(msg)
-        object.__setattr__(self, name, value)
+        """Only allow first_name attribute"""
+        if name == 'first_name':
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(name)
+            )
