3a15ba95ccb26acbe16f3e6f35341d6c6275e9c8
iradufashagratien <g.iradufash@alustudent.com>
2026-09-20 17:04:18 +0000

    Add -L to follow redirects on header request

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request with a custom header, displays the body, retries if server isn't ready yet
-curl -s --retry 5 --retry-delay 1 -H "X-HolbertonSchool-User-Id: 98" "$1"
+# sends a GET request with a custom header, follows redirects, displays the body
+curl -sL --retry 5 --retry-delay 1 --header "X-HolbertonSchool-User-Id: 98" "$1"
